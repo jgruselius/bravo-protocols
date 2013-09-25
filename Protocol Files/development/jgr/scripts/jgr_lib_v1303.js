8b77cd9df4bea7510479ac4dae067db026cd0d3b
Joel Gruselius <joel.gruselius@scilifelab.se>
2013-09-25 14:44:37 +0200

    Update jgr_lib_v1303.js
    Numerical plate ID's are now sorted numerically rather than alphabetically in function parseTransfers.

--- Protocol Files/development/jgr/scripts/jgr_lib_v1303.js
+++ Protocol Files/development/jgr/scripts/jgr_lib_v1303.js
@@ -221,10 +221,15 @@ function parseCsv(str, delim) {
 */
 function parseTransfers(str) {
 	var rowArray = parseCsv(str, ",");
-	// Sort the array alphabetically by plate ID:
+	// Sort the array alphabetically (alt. numerically) by plate ID:
 	rowArray.sort(function(row1, row2) {
 		var a_id = row1[0].toLowerCase();
 		var b_id = row2[0].toLowerCase();
+		// If plate ID is a number, convert from string to int/double:
+		if(isNumber(a_id) && isNumber(b_id)) {
+			a_id = readNumeral(a_id);
+			b_id = readNumeral(b_id);
+		}
 		if(a_id !== b_id) {
 			return (a_id < b_id) ? -1 : (a_id > b_id ? 1 : 0);
 		} else {
