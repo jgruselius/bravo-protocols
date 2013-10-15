42960406c56c1bb65c152d70493d53fa330ddf37
Joel Gruselius <joel.gruselius@scilifelab.se>
2013-10-15 15:07:56 +0200

    Added print-version and updated tartrazine assay protocol

---
+++ Protocol Files/development/jgr/print-repo-version.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "C:\VWorks Workspace\"
+git rev-parse --abbrev-ref HEAD > C:\Users\Administrator\Desktop\VERSION.txt
+echo. >> C:\Users\Administrator\Desktop\VERSION.txt
+git rev-parse HEAD >> C:\Users\Administrator\Desktop\VERSION.txt
