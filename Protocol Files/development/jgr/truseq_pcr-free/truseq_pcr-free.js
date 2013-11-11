7019caa6a3026c7c921726458ae2ce9241b74190
Joel Gruselius <joel.gruselius@scilifelab.se>
2013-11-11 12:02:35 +0100

    Added settigs for insert size to PCR-free

--- Protocol Files/development/jgr/truseq_pcr-free/truseq_pcr-free.js
+++ Protocol Files/development/jgr/truseq_pcr-free/truseq_pcr-free.js
@@ -14,7 +14,8 @@ presets["A-tailing"] = {tipColumn:2,reagentColumn:2,sampleVolume:15,reagentVolum
 presets["Ligation"] = {tipColumn:3,reagentColumn:3,sampleVolume:30,reagentVolume:5,bufferVolume:5,adapterVolume:2.5,incubationTemperature:30,incubationTime:600,doOffDeckIncubation:true};
 
 presets["Fragmentation cleanup"] = {sampleVolume:50,beadVolume:80,elutionVolume:50};
-presets["Size selection"] = {sampleVolume:100,beadVolume1:95,beadVolume2:30,bindVolume:160,transferVolume:150,elutionVolume:15};
+presets["Size selection 350 bp"] = {sampleVolume:100,beadVolume1:95,beadVolume2:30,bindVolume:160,transferVolume:150,elutionVolume:15};
+presets["Size selection 550 bp"] = {sampleVolume:100,beadVolume1:80,beadVolume2:30,bindVolume:160,transferVolume:150,elutionVolume:15};
 presets["Ligation cleanup 1"] = {sampleVolume:42.5,beadVolume:42.5,elutionVolume:50};
 presets["Ligation cleanup 2"] = {sampleVolume:50,beadVolume:50,elutionVolume:20};
 
@@ -25,7 +26,8 @@ fileNames["End repair"] = "truseq_pcr-free_reaction.pro";
 fileNames["A-tailing"] = "truseq_pcr-free_reaction.pro";
 fileNames["Ligation"] = "truseq_pcr-free_ligation.pro";
 fileNames["Fragmentation cleanup"] = "illumina_spri.pro";
-fileNames["Size selection"] = "illumina_double-spri.pro";
+fileNames["Size selection 350 bp"] = "illumina_double-spri.pro";
+fileNames["Size selection 550 bp"] = "illumina_double-spri.pro";
 fileNames["Ligation cleanup 1"] = "illumina_spri.pro";
 fileNames["Ligation cleanup 2"] = "illumina_spri.pro";
 fileNames["Library prep"] = "truseq_pcr-free.rst";
@@ -35,10 +37,14 @@ var runsetOrder = [];
 
 if(formProtocol === "Library prep") {
 	runsetMode = true;
-	runsetOrder = ["Fragmentation cleanup","End repair","Size selection",
+	runsetOrder = ["Fragmentation cleanup","End repair","Size selection "+formInsertSize,
 			"A-tailing","Ligation","Ligation cleanup 1","Ligation cleanup 2"];
 	runset.openRunsetFile(path+fileNames[formProtocol], form);
 	updateSettings("End repair");
+} else if(formProtocol === "Size selection") {
+	runsetMode = false;
+	runset.appendProtocolFileToRunset(path+fileNames[formProtocol+" "+formInsertSize], 1, "", form);
+	updateSettings(formProtocol+" "+formInsertSize);
 } else if(formProtocol === "Ligation cleanup") {
 	runsetMode = true;
 	runsetOrder = ["Ligation cleanup 1","Ligation cleanup 2"];
