c5cbc362f0a294e4c742438d2c8ed5de8f5e55a6
Joel Gruselius <joel.gruselius@scilifelab.se>
2014-11-25 08:09:43 +0100

    More robust parsing of plate coords in transfer protocols
    Changed to regex-based parsing of plate coordinates in all modes of the
    transfer protocols

--- Protocol Files/facility/transfer/transfer_lib.js
+++ Protocol Files/facility/transfer/transfer_lib.js
@@ -263,9 +263,9 @@ function parseTransfers(str) {
 		var sourcePlate, sourcePlateIndex, sourceWell, volume, destinationWell;
 		try {
 			sourcePlate = row[0];
-			sourceWell = convertCoords(row[1]);
+			sourceWell = convertCoordsRegExp(row[1]);
 			volume = parseNumber(row[2], 3);
-			destinationWell = convertCoords(row[3]);
+			destinationWell = convertCoordsRegExp(row[3]);
 		} catch(e) {
 			throw "UnableToParseTransferTableException:" + e;
 		}
@@ -330,7 +330,7 @@ function parseAdapterTransfers(str, indexSet) {
 		try {
 			source = convertCoords(plateMap[row[1]]);
 			volume = parseNumber(row[2], 3);
-			destination = convertCoords(row[0]);
+			destination = convertCoordsRegExp(row[0]);
 		} catch(e) {
 			throw "UnableToParseTransferTableException:" + e;
 		}
