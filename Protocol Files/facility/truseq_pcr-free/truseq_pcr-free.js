29d79031c37b0d1d42c58d3ebe695713b2f2c469
Joel Gruselius <joel.gruselius@scilifelab.se>
2015-05-29 16:06:54 +0200

    Minor fixes

--- Protocol Files/facility/truseq_pcr-free/truseq_pcr-free.js
+++ Protocol Files/facility/truseq_pcr-free/truseq_pcr-free.js
@@ -93,7 +93,7 @@ fileNames["Ligation cleanup 1"] = "illumina_spri.pro";
 fileNames["Ligation cleanup 2"] = "illumina_spri.pro";
 fileNames["Library prep"] = "truseq_pcr-free.rst";
 fileNames["Ligation cleanup"] = "truseq_pcr-free_cleanup.rst";
-fileNames["qPCR setup"] = "../qpcr-384/qpcr-384_setup_ver2.pro";
+fileNames["qPCR setup"] = "../qpcr-384/qpcr-384_setup_ver3.pro";
 
 var runsetOrder = [];
 
