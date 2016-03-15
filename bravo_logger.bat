efba47722fcb3b6b7e6a70096be3e59d354656c1
Joel Gruselius <joel.gruselius@scilifelab.se>
2016-03-15 15:16:50 +0100

    Changed log path to network share

--- bravo_logger.bat
+++ bravo_logger.bat
@@ -1,11 +1,12 @@
 @ECHO OFF
 SETLOCAL
-SET LOG="%USERPROFILE%\Desktop\bender_errors.log"
+SET LOG_PATH="Z:\bravo_logs\"
+SET LOG_FILE="bender_errors.log"
+IF NOT EXIST %LOG_PATH% (SET LOG_PATH="%USERPROFILE%\Desktop\")
 SET SCRIPT="%USERPROFILE%\Repos\standalone_scripts\upload_robot_logs.py"
 SET STB_CONF="%USERPROFILE%\Repos\statusdb.yaml"
 SET STB_LOG="%USERPROFILE%\Repos\statusdb_upload.log"
 FOR /F "TOKENS=2-4 DELIMS=/ " %%A IN ('DATE /T') DO (SET TODAY=%%C-%%A-%%B)
 FOR /F "TOKENS=1-2 DELIMS=/:" %%A IN ("%TIME%") DO (SET NOW=%%A:%%B)
-IF NOT EXIST %LOG% TYPE NUL > %LOG%
-ECHO %TODAY% %NOW% %* >> %LOG%
+ECHO %TODAY% %NOW% %* >> %LOG_PATH%%LOG_FILE%
 ECHO %* | python %SCRIPT% --name "Bender" --conf %STB_CONF% --logfile %STB_LOG%
